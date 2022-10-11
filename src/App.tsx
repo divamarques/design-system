96f9dc3e0362498011ca90f9904f1ac7b013e722
Diogo V Marques <dimarques@gmail.com>
2022-10-11 20:33:24 -0300

    Instalação storybook - ajuste de configuração do tailwind.config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import './styles/global.css';
+
 export function App() {
   return (
-    <h1>Olá que tal?</h1>
+    <>
+      <h1 className='font-bold text-5xl text-violet-500'>Olá que tal?</h1>
+      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-lime-300'>
+        Enviar
+      </button>
+    </>
   )
 }
 
